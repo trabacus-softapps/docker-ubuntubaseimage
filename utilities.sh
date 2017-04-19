47970ef490254128135fe9c098a0a8670cba52ab
Arun T K <softapps@users.noreply.github.com>
2017-04-19 17:11:46 +0530

    fixed - failed attempt to execute pdftotext

--- utilities.sh
+++ utilities.sh
@@ -3,7 +3,7 @@ set -e
 source /pd_build/buildconfig
 
 ## Many NPM packages contain native extensions and require a compiler.
-run minimal_apt_get_install build-essential libpq-dev libxrender1 fontconfig xvfb sudo 
+run minimal_apt_get_install build-essential libpq-dev libxrender1 fontconfig xvfb sudo poppler-utils 
 run minimal_apt_get_install postgresql-client-9.6 nodejs 
 run npm install -g less less-plugin-clean-css
 run npm cache clear
