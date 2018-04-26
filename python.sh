b0e40066871d31969aadcac3bb3150e5764cbfa5
Arun T K <arun.kalikeri@softappsit.com>
2018-04-26 10:52:09 +0530

    fixed error: no such option: --use-wheel

--- python.sh
+++ python.sh
@@ -7,7 +7,7 @@ header "Installing Python..."
 ## Install Python.
 run apt-get install -y python python-dev
 curl https://bootstrap.pypa.io/get-pip.py | python
-run pip install --upgrade --use-wheel --no-index --pre \
+run pip install --upgrade --no-index --pre \
         --find-links=/pd_build/lib  --requirement=/pd_build/requirements.txt
 
 ## fixed “libGeoIP.so.1: cannot open shared object No such file or directory” error
