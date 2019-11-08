60b6c796803e60942a4fef6cb4a8e1010d3a6eaf
Arun T K <softapps@users.noreply.github.com>
2019-11-08 12:39:27 +0530

    Update utilities.sh

--- utilities.sh
+++ utilities.sh
@@ -3,12 +3,12 @@ set -e
 source /pd_build/buildconfig
 
 ## Many NPM packages contain native extensions and require a compiler.
-run minimal_apt_get_install build-essential libpq-dev libxrender1 fontconfig xvfb sudo poppler-utils 
+run minimal_apt_get_install build-essential libpq-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg-dev libfreetype6-dev zlib1g-dev libldap2-dev libsasl2-dev libxrender1 fontconfig xvfb sudo poppler-utils 
 run minimal_apt_get_install postgresql-client-9.6 nodejs 
 run npm install -g less less-plugin-clean-css
 run npm cache clear
-curl -Lf -o /tmp/wkhtmltox-0.12.4-amd64.tar.xz https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+curl -Lf -o /tmp/wkhtmltox_0.12.5-1.xenial_amd64.deb https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.xenial_amd64.deb
 run cd /opt/
-run sudo tar xf /tmp/wkhtmltox-0.12.4-amd64.tar.xz 
+run minimal_apt_get_install /tmp/wkhtmltox_0.12.5-1.xenial_amd64.deb 
 run sudo ln -s /opt/wkhtmltox/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
 run sudo ln -s /opt/wkhtmltox/bin/wkhtmltoimage /usr/bin/wkhtmltoimage
