2ae572f3a4e8d9d01f576cfef9cfdbd01b005ce9
Ubuntu <ubuntu@ip-172-31-27-42.ap-south-1.compute.internal>
2020-01-07 02:00:31 +0000

    fixed geoip

--- utilities.sh
+++ utilities.sh
@@ -17,6 +17,6 @@ run sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin/wkhtmltoimage
 #
 # We only use the city db, you can add the country db and others if you want them.
 run mkdir -p /usr/share/geoip 
-curl -Lf -o /tmp/city.tar.gz http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+run cp /pd_build/geolite2/GeoLite2-City_20200107.tar.gz /tmp/city.tar.gz
 run tar xf /tmp/city.tar.gz -C /usr/share/geoip --strip 1
 run ls -al /usr/share/geoip/
