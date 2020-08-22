90a4f00b1f588720b123b4ac82c83b5a569d7dcf
Arun T K <softapps@users.noreply.github.com>
2020-08-22 22:28:00 +0530

    fixed typo error

--- enable_repos.sh
+++ enable_repos.sh
@@ -7,7 +7,7 @@ header "Preparing APT repositories"
 ## PostgreSQL Latest
 run apt-get update
 run minimal_apt_get_install gnupg2 curl
-run apt-key adv --keyserver hpk://keyserver.ubuntu.com:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
+run apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
 echo "+ Enabled PostgreSQL APT repo"
 echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 
