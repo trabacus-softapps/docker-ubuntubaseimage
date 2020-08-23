11d52f70e486d513a6f226fb68723c9845b0c302
Arun T K <softapps@users.noreply.github.com>
2020-08-23 17:55:51 +0530

    Update finalize.sh

--- finalize.sh
+++ finalize.sh
@@ -4,8 +4,8 @@ source /pd_build/buildconfig
 
 header "Finalizing..."
 
-run apt-get remove -y autoconf automake
-run apt-get autoremove
-run apt-get clean
-run rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-run rm -rf /pd_build
+run apt-get remove -y autoconf automake \
+&& apt-get autoremove \
+&& apt-get clean \
+&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+&& rm -rf /pd_build
