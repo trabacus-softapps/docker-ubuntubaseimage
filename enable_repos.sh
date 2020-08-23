6631cdcb5fedf058ae4a6982ee0aa7ba335598a3
Arun T K <softapps@users.noreply.github.com>
2020-08-23 17:58:17 +0530

    Update enable_repos.sh

--- enable_repos.sh
+++ enable_repos.sh
@@ -5,10 +5,10 @@ source /pd_build/buildconfig
 header "Preparing APT repositories"
 
 ## PostgreSQL Latest
-run apt-get update
-run apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
-echo "+ Enabled PostgreSQL APT repo"
-echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+run apt-get update \
+&& apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8 \
+&& echo "+ Enabled PostgreSQL APT repo" \
+&& "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list \
 
 ## Nginx Latest
 ##run apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62 
@@ -16,6 +16,6 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/
 ##echo "deb http://nginx.org/packages/ubuntu/ bionic nginx" > /etc/apt/sources.list.d/nginx.list
 
 ## Nodejs Latest
-curl -sL https://deb.nodesource.com/setup_12.x | bash -
-run apt-get update
-run apt-get upgrade -y -o Dpkg::Options::="--force-confold"
+&& curl -sL https://deb.nodesource.com/setup_12.x | bash - \
+&& apt-get update \
+&& apt-get upgrade -y -o Dpkg::Options::="--force-confold"
