64e5eba331d09fe608ac28807ffba30d895cdaa4
Ubuntu <ubuntu@dev01.zoowei.app>
2024-06-19 19:41:04 +0000

    fixed postgresql error

--- enable_repos.sh
+++ enable_repos.sh
@@ -2,14 +2,15 @@
 set -e
 source /pd_build/buildconfig
 
-run minimal_apt_get_install gnupg curl
+run minimal_apt_get_install gnupg curl ca-certificates
+run apt-get update
 
 header "Preparing APT repositories"
 
 ## PostgreSQL Latest
 run apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
 echo "+ Enabled PostgreSQL APT repo"
-echo "deb http://apt-archive.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+echo "deb https://apt-archive.postgresql.org/pub/repos/apt bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 
 run apt-get update
 run apt-get upgrade -y -o Dpkg::Options::="--force-confold"
