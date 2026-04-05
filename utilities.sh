b4fe9da0d83ef270908a2da5ab2bfacb7e8017d2
Ubuntu <ubuntu@node-02.reboltnetwork.com>
2026-04-05 20:47:22 +0000

    changed os and lib

--- utilities.sh
+++ utilities.sh
@@ -4,8 +4,9 @@ source /pd_build/buildconfig
 
 ## Many NPM packages contain native extensions and require a compiler.
 run minimal_apt_get_install build-essential sudo ca-certificates dirmngr fonts-noto-cjk xz-utils xz-utils libssl-dev libxml2-dev libxslt1-dev libjpeg-dev libfreetype6-dev zlib1g-dev libldap2-dev libsasl2-dev build-essential postgresql-client sudo xfonts-75dpi xfonts-base fontconfig libjpeg62-turbo libx11-6 libxcb1 libxext6 libxrender1 xfonts-75dpi xfonts-base \
-&& curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - \
+&& curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - \
 && apt-get install nodejs -y \
+&& set -x && npm install -g less less-plugin-clean-css rtlcss \
 && curl -Lf -o /tmp/wkhtmltox_bullseye_amd64.deb https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bullseye_amd64.deb \
 && cd /opt/ \
 && minimal_apt_get_install /tmp/wkhtmltox_bullseye_amd64.deb
